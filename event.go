e569be6b96b56448daf46633fdf1dc881366750b
as <as.utf8@gmail.com>
2017-09-04 18:33:48 -0700

    new events

--- event.go
+++ event.go
@@ -1,47 +1,56 @@
 package event
 
 import (
-	"fmt"
 	"bytes"
+	"fmt"
 	"unicode"
 )
 
-type Record interface{
+type Editor interface {
+	Insert(p []byte, at int64) (n int)
+	Delete(q0, q1 int64) (n int)
+	Len() int64
+	Bytes() []byte
+	Select(q0, q1 int64)
+	Dot() (q0, q1 int64)
+}
+
+type Record interface {
 	String() string
-//	Equal(Record) bool
+	//	Equal(Record) bool
 	Coalesce(Record) bool
-//	Invert(Record) Record
+	//	Invert(Record) Record
 }
 
 type Event interface {
 	isevent()
 }
 
-type Rec struct{
-	ID int
-	Kind byte
+type Rec struct {
+	ID     int
+	Kind   byte
 	Q0, Q1 int64
-	N int64
-	P []byte
+	N      int64
+	P      []byte
 }
 
-func (r Rec) String() string{
-	return fmt.Sprintf("%d	%x	%d	%d	%d	%q\n",r.ID, r.Kind, r.Q0,r.Q1,r.N,r.P)
+func (r Rec) String() string {
+	return fmt.Sprintf("%d	%x	%d	%d	%d	%q\n", r.ID, r.Kind, r.Q0, r.Q1, r.N, r.P)
 }
-func (r Rec) Record() (p []byte, err error){
+func (r Rec) Record() (p []byte, err error) {
 	return []byte(r.String()), nil
 }
 
-func (r Insert) Record() (p []byte, err error){
+func (r Insert) Record() (p []byte, err error) {
 	r.Kind = 'i'
 	r.N = int64(len(p))
 	return r.Rec.Record()
 }
-func (r Delete) Record() (p []byte, err error){
+func (r Delete) Record() (p []byte, err error) {
 	r.Kind = 'd'
 	return r.Rec.Record()
 }
-func (r Select) Record() (p []byte, err error){
+func (r Select) Record() (p []byte, err error) {
 	r.Kind = 's'
 	return r.Rec.Record()
 }
@@ -49,26 +58,27 @@ func (r Select) Record() (p []byte, err error){
 type Insert struct {
 	Rec
 }
-func space(b byte) int{
-	if unicode.IsSpace(rune(b)){
+
+func space(b byte) int {
+	if unicode.IsSpace(rune(b)) {
 		return 1
 	}
 	return 0
 }
 
-func (e *Insert) Coalesce(v Record) bool{
-	if v == nil{
+func (e *Insert) Coalesce(v Record) bool {
+	if v == nil {
 		return false
 	}
-	switch v := v.(type){
+	switch v := v.(type) {
 	case *Insert:
-		if len(v.P) == 0{
+		if len(v.P) == 0 {
 			return true
 		}
-		if v.ID != e.ID{
+		if v.ID != e.ID {
 			return false
 		}
-		if (space(v.P[0]) != space(e.P[0])) {
+		if space(v.P[0]) != space(e.P[0]) {
 			return false
 		}
 		if v.Q0 == e.Q1 {
@@ -77,16 +87,16 @@ func (e *Insert) Coalesce(v Record) bool{
 			return true
 		}
 	case *Delete:
-		if v.ID != e.ID{
+		if v.ID != e.ID {
 			return false
 		}
 		if len(v.P) == 1 && len(e.P) == 1 && (space(v.P[0]) != space(e.P[0])) {
 			return false
 		}
-		if e.Q0 >= v.Q0 && v.Q1 == e.Q1-1{
-		   // 0      3        3       4
-			e.Q1 -= v.Q1-v.Q0
-			e.P = e.P[:e.Q1]
+		if e.Q0 >= v.Q0 && v.Q1 == e.Q1-1 {
+			// 0      3        3       4
+			e.Q1 -= v.Q1 - v.Q0
+			e.P = e.P[:min(int64(len(e.P)), e.Q1)]
 			return true
 		}
 	case *Select:
@@ -95,46 +105,53 @@ func (e *Insert) Coalesce(v Record) bool{
 	return false
 }
 
-func (e *Delete) Coalesce(v Record) bool{
-	switch v :=v.(type){
+func min(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func (e *Delete) Coalesce(v Record) bool {
+	switch v := v.(type) {
 	case *Insert:
 		return false
 		// Works better to just return false here
 		//
 		//
-		if v.ID != e.ID{
+		if v.ID != e.ID {
 			return false
 		}
-		if v.Q0 != e.Q0{
+		if v.Q0 != e.Q0 {
 			return false
 		}
-		if v.Q1 > e.Q1+1{
+		if v.Q1 > e.Q1+1 {
 			return false
 		}
 		e.Q0 = v.Q1
-		if len(e.P) == 0 || bytes.Equal(e.P[:e.Q0], v.P){
+		if len(e.P) == 0 || bytes.Equal(e.P[:e.Q0], v.P) {
 			return false
 		}
 		copy(e.P, e.P[e.Q0:])
-		e.P=e.P[:(e.Q1-e.Q0)+1]
+		e.P = e.P[:(e.Q1-e.Q0)+1]
 		return true
 	case *Delete:
-		if v.ID != e.ID{
+		if v.ID != e.ID {
 			return false
 		}
-		if e.Q0 != v.Q1{
+		if e.Q0 != v.Q1 {
 			return false
 		}
-			e.Q0 = v.Q0
-			e.P = append(v.P, e.P...)
+		e.Q0 = v.Q0
+		e.P = append(v.P, e.P...)
 		return true
 	}
 	return false
 }
-func (e *Select) Coalesce(v Record) bool{
-	switch v := v.(type){
+func (e *Select) Coalesce(v Record) bool {
+	switch v := v.(type) {
 	case *Select:
-		if v.Q0 == e.Q0 && v.Q1 == e.Q1{
+		if v.Q0 == e.Q0 && v.Q1 == e.Q1 {
 			return true
 		}
 	}
@@ -162,7 +179,32 @@ type Sweep struct {
 }
 type Move struct {
 }
+type Cmd struct {
+	Rec
+	From     Editor
+	To       []Editor
+	FromFile string
+}
+type Look struct {
+	Rec
+	From     Editor
+	To       []Editor
+	FromFile string
+}
 
+type Get struct {
+	ID    int
+	Name  string
+	Path  string
+	Addr  string
+	IsDir bool
+}
+type Put struct {
+	ID int
+}
+
+func (Look) isevent()      {}
+func (Cmd) isevent()       {}
 func (Insert) isevent()    {}
 func (Delete) isevent()    {}
 func (Select) isevent()    {}
@@ -172,3 +214,5 @@ func (Scroll) isevent()    {}
 func (Redraw) isevent()    {}
 func (Sweep) isevent()     {}
 func (Move) isevent()      {}
+func (Get) isevent()       {}
+func (Put) isevent()       {}
